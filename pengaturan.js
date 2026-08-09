0a4094033e3d43aa7d99efdd527457ebe763e686
Storyvisual-id <yasa98022@gmail.com>
2026-08-09 09:37:11 +0800

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -9,5 +9,5 @@ const CONFIG_APP = {
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyDakCOpA43YHMLI2y78isEmAppQrn2icFs",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "087849352093" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
